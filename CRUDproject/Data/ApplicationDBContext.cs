079ec7922c66193bfaadaf619915195c4532d2c8
Hadymohammed <Hadymohamed2002@gmail.com>
2023-04-27 04:44:52 +0200

    add database,categoryModel

---
+++ CRUDproject/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUDproject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDproject.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options):base(options)
+        {
+        }
+
+        public DbSet<Category> categories { get; set; }
+    }
+}
---
+++ CRUDproject/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDproject.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Displayname { get; set; }
+        public DateTime createdDate { get; } = DateTime.Now;
+    }
+}
