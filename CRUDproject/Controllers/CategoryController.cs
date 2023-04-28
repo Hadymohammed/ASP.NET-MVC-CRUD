05b0cd81a47ce0a196e5ff526baea119506fc602
Hadymohammed <Hadymohamed2002@gmail.com>
2023-04-28 11:50:51 +0300

    add categoryView

---
+++ CRUDproject/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using CRUDproject.Data;
+using CRUDproject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDproject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.categories;
+            return View(categories);
+        }
+    }
+}
